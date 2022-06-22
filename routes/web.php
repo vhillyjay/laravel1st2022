4abb48707cb11a31df21ef0be8bb3ec410ebb6fb
vhillyjay <107127737+vhillyjay@users.noreply.github.com>
2022-06-22 16:00:28 +0800

    layout folder

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,20 @@ Route::get('/', function () {
 Route::get('/supplies', function () {
     $schoolSupplies = [
         'type' => 'pencil',
-        'brand' => 'Mongol',
+        'brand' => 'HBW',
         'price' => 10
     ]; // variable to be passed on view
-    return view('supplies', $schoolSupplies); // variable from above
+    return view('supplies', $schoolSupplies); // page on views, variable from above
     // return "booksAndSupplies"; // text/html
     // return ["name" => "theChronicle", "type" => "books"]; // application/json
 });
 // routes shown on url
+
+// Route::get('/supplies', function () { // /supplies for url
+//     $supplies = [
+//         ['type' => 'pencil', 'brand' => 'HBW'],
+//         ['type' => 'eraser', 'brand' => 'HBW'],
+//         ['type' => 'paper', 'brand' => 'National Bookstore']
+//     ];
+//     return view('supplies', ['supplies' => $supplies]); // supplies from views folder
+// });
